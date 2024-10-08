a58e72d6a36f4d966c20bfff528181a11af9ae59
Nathaniel Mon Père <47852617+natevmp@users.noreply.github.com>
2024-10-08 16:19:22 +0100

    Adjusted pde example
    change pde example to include treatment dependent death rate and logistic growth rate

--- test/fitnessDistDynamicsMOL.jl
+++ test/fitnessDistDynamicsMOL.jl
@@ -1,33 +1,41 @@
 using ModelingToolkit, MethodOfLines, OrdinaryDiffEq, DomainSets
 
-##
-
 # Parameters, variables, and derivatives
 @parameters t x
-@parameters γ, w
+
 @variables n(..)
 Dt = Differential(t)
 Dx = Differential(x)
 Dxx = Differential(x)^2
 
+# parameters
+w=0.1
+b0 = 1.2
+d0 = 0.2
+K = 1000
+ri = 0.
+α = 10.
+h = 1.
+
+# birth and death rates
+birthRate(n,b0,K) = b0*(1 − n/K)
+deathRate(h,u,d0,ri,α) = d0 + h/(ri + u*α)
+
 # 1D PDE and boundary conditions
-eq  = Dt(n(t, x)) ~ γ*x*n(t,x) + w/2 * Dxx(n(t, x))
+eq  = Dt(n(t, x)) ~ 
+    (birthRate(n(t,x), b0, K) - deathRate(h, x, d0, ri, α))*n(t,x) + w/2 * Dxx(n(t, x))
 bcs = [n(0, x) ~ 1.,
         Dx(n(t, 0)) ~ 0.0,
         Dx(n(t, 1)) ~ 0.0]
 
 # Space and time domains
-domains = [t ∈ Interval(0.0, 10.0),
-        x ∈ Interval(0.0, 1.0)]
+domains = [t ∈ (0.0, 10.0),
+        x ∈ (0.0, 1.0)]
 
 # PDE system
-@named pdesys = PDESystem(eq, bcs, domains, [t, x],[n(t, x)], [γ=>0.1, w=>1.0])
-
-dx = 0.01
-discretization = MOLFiniteDifference([x => dx], t)
+@named pdesys = PDESystem(eq, bcs, domains, [t, x], [n(t, x)])
 
 # Method of lines discretization
-# Need a small dx here for accuracy
 dx = 0.01
 # order = 2
 discretization = MOLFiniteDifference([x => dx],t)
@@ -39,16 +47,14 @@ prob = discretize(pdesys, discretization)
 using OrdinaryDiffEq
 sol = solve(prob, Tsit5(), saveat=0.2)
 
+##
+
+using CairoMakie
 
 # Plot results and compare with exact solution
 discrete_x = sol[x]
 discrete_t = sol[t]
-
 soln = sol[n(t, x)]
-
-##
-
-using CairoMakie
 
 fig1 = Figure()
 Axis(
@@ -56,5 +62,8 @@ Axis(
     xlabel="phenotype",
     ylabel="density of cells",
 )
-lines!(discrete_x, soln[25,:])
+for t in [1,10,20,30,40,50]
+    lines!(discrete_x, soln[t,:], label="time $t")
+end
+axislegend(position=:lt)
 display(fig1)
